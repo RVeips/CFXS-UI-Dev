e3da0029498fdec90bf78dab3a8e4dbed70e8e5d
RVeips <rihardsveips@gmail.com>
2021-12-20 00:48:34 +0200

    test cfg done
    Signed-off-by: RVeips <rihardsveips@gmail.com>

--- APG.cfg.lua
+++ APG.cfg.lua
@@ -1,11 +1,38 @@
 return {
-    ["ProjectName"] = "CFXS-UI-Dev",
-    ["CPU"] = "TM4C1294NCPDT",
-    ["Modules"] = {
+    ProjectName = "CFXS UI Dev",
+    
+    Toolchain = "GCC",
+    UnityBuildBatchSize = 16,
+
+    CPU = "TM4C1294NCPDT",
+    CLOCK_FREQUENCY = 120e6,
+    
+    Modules = {
         "CMake",
         "CFXS-Base",
         --"CFXS-Platform",
         "CFXS-HW",
-        "TM4C-driverlib"
-    }
+        "tm4c-driverlib",
+        "LicenseGen",
+        "SeggerRTT"
+    },
+
+    IncludeDirectories = {
+        "/src"
+    },
+
+    Defines = {
+        "debug:DEBUG",
+        "debug:CFXS_DEBUG",
+        "debug:CFXS_DEBUG_ASSERT",
+        
+        "release:RELEASE",
+        "release:NDEBUG",
+    },
+
+    LicenseHeader = table.concat({
+        '// [CFXS] //',
+        '${ProjectNameRaw}',
+        '${Year} | CFXS / Rihards Veips'
+    }, '\n')
 }
