fc049793234e52adf35433cd499039731f3be207
RVeips <rihardsveips@gmail.com>
2021-12-19 21:52:53 +0200

    more APG tests
    Signed-off-by: RVeips <rihardsveips@gmail.com>

--- APG.cfg.lua
+++ APG.cfg.lua
@@ -1,3 +1,11 @@
 return {
     ["ProjectName"] = "CFXS-UI-Dev",
+    ["CPU"] = "TM4C1294NCPDT",
+    ["Modules"] = {
+        "CMake",
+        "CFXS-Base",
+        --"CFXS-Platform",
+        "CFXS-HW",
+        "TM4C-driverlib"
+    }
 }
